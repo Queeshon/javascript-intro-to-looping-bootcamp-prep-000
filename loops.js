748c126f246f6bd088a9f43ac62e0dfc9aa21771
Christian Kim <christiandwkim@gmail.com>
2018-01-05 19:01:31 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,5 @@
 function forLoop(array) {
-  const strangeArray = forLoop(array)
-  for (var i = 0; l = testArray.length; i < l; i++){
+  for (var i = 0; i < 25; i++){
     return array.unshift ('I am ${i} strange loops.')
   }
 }
